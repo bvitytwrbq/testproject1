4d0e9b0337c4bf95fd0bca67ab54dc46f99fd3c3
bvitytwrbq <bvitytwrbq@gmail.com>
2025-10-31 09:05:49 +0100

    bla2

--- app/src/main/java/testproject1/App.java
+++ app/src/main/java/testproject1/App.java
@@ -5,7 +5,7 @@ package testproject1;
 
 public class App {
     public String getGreeting() {
-        return "Hello World!";
+        return "banana";
     }
 
     public static void main(String[] args) {
