7aff5009da024b793571dcc0530176d51c1addc1
Lisha-0 <lishayadav.017@gmail.com>
2025-11-03 11:39:32 +0100

    potato

--- app/src/main/java/testproject1/App.java
+++ app/src/main/java/testproject1/App.java
@@ -5,7 +5,7 @@ package testproject1;
 
 public class App {
     public String getGreeting() {
-        return "tomato";
+        return "potato";
     }
 
     public static void main(String[] args) {
